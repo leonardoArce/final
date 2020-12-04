e8a57072a8e2ac65d95e75ca993f8d8c53f178a6
leonardoArce <72895619+leonardoArce@users.noreply.github.com>
2020-12-04 00:29:03 -0800

    Add files via upload

---
+++ NuevaC/ap/authController.php
@@ -0,0 +1,145 @@
+
+<?php 
+
+
+include_once "app.php";
+include_once "connectionController.php";
+
+if (isset($_POST['action'])) {
+
+	if (isset($_POST['token']) && $_POST['token']==$_SESSION['token']) { 
+
+		$authController = new AuthController();
+
+		switch ($_POST['action']) {
+			case 'register':
+				
+				$name = strip_tags($_POST['name']);
+				$email = strip_tags($_POST['email']);
+				$password = strip_tags($_POST['password']);
+
+				$authController->register($name,$email,$password);
+
+			break;
+			case 'login':
+				
+				$email = strip_tags($_POST['email']);
+				$password = strip_tags($_POST['password']);
+
+				$authController->access($email,$password);
+
+			break; 
+		}
+
+	}else{
+
+		$_SESSION['error'] = 'de seguridad';
+		header("Location:". $_SERVER['HTTP_REFERER'] );
+	}
+}
+
+class AuthController
+{
+
+	public function register($name,$email,$password)
+	{
+		$conn = connect();
+		if (!$conn->connect_error) { 
+
+			if ($name!="" && $email != "" && $password != "") {
+				
+				$originalPassword = $password;
+				$password = sha1($password.'wakwak_eee_123');
+
+				$query = "insert into users (name, email,password) value (?,?,?)";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->bind_param('sss',$name,$email,$password);
+
+				if ($prepared_query->execute()) {
+					
+					$this->access($email,$originalPassword);
+
+				}else{
+
+					$_SESSION['error'] = 'verifique los datos envíados';
+
+					header("Location:". $_SERVER['HTTP_REFERER'] );
+				}
+
+
+			}else{
+				$_SESSION['error'] = 'verifique la información del formulario';
+
+				header("Location:". $_SERVER['HTTP_REFERER'] );
+			}
+
+
+
+		}else{
+			$_SESSION['error'] = 'verifique la conexión a la base de datos';
+
+			header("Location:". $_SERVER['HTTP_REFERER'] );
+		}
+	}
+
+	public function access($email,$password)
+	{
+		$conn = connect();
+		if (!$conn->connect_error) {
+			if ($email!="" && $password!="") {
+				$password = sha1($password.'wakwak_eee_123');
+
+				$query = "select * from users where email = ? and password = ?";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->bind_param('ss',$email,$password);
+
+				if ($prepared_query->execute()) {
+					
+					$results = $prepared_query->get_result(); 
+
+					$user = $results->fetch_all(MYSQLI_ASSOC);
+
+					if (count($user)>0) {
+						
+						$user = array_pop($user);
+
+						$_SESSION['id'] = $user['id'];
+						$_SESSION['name'] = $user['name'];
+						$_SESSION['email'] = $user['email'];  
+
+
+						header("Location:".BASE_PATH."categories" );
+					}else{
+						$_SESSION['error'] = 'verifique los datos envíados'; 
+						header("Location:". $_SERVER['HTTP_REFERER'] );
+					}
+
+
+				}else{
+					$_SESSION['error'] = 'verifique los datos envíados';
+
+					header("Location:". $_SERVER['HTTP_REFERER'] );
+				}
+			}else{
+				$_SESSION['error'] = 'verifique la información del formulario';
+
+				header("Location:". $_SERVER['HTTP_REFERER'] );
+			}
+		}else{
+			$_SESSION['error'] = 'verifique la conexión a la base de datos'; 
+
+			header("Location:". $_SERVER['HTTP_REFERER'] );
+		}  
+
+	}
+
+	public function logout()
+	{
+
+	}
+
+}
+
+
+
+?>
---
+++ NuevaC/cate/index.php
@@ -0,0 +1,232 @@
+<?php 
+	
+	include "../ap/categoryController.php";
+	$categoryController = new CategoryController();
+
+	$categories = $categoryController->get();
+
+	if (isset($_SESSION)==false || 
+		isset($_SESSION['id'])==false) {
+		
+		header("Location:../");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Categories
+	</title>
+	<style type="text/css">
+		table, th, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		#updateForm{
+			display: none;
+		}
+	</style>
+</head>
+<body>
+	<div>
+
+		<h1>
+			Categories
+		</h1>
+
+		<?php 
+
+		if (isset($_SESSION) && isset($_SESSION['error'])) {
+
+			echo "<h3> Error: ".$_SESSION['error']."</h3>";
+			unset($_SESSION['error']);
+
+		}
+
+		?> 
+
+		<?php include "../layouts/alerts.template.php"; ?>
+
+		<table>
+			<thead>
+				<th>
+					#
+				</th>
+				<th>
+					Name
+				</th>
+				<th>
+					Description
+				</th>
+				<th>
+					Actions
+				</th>
+			</thead>
+			<tbody>
+
+				<?php foreach ($categories as $category): ?>
+
+				<tr>
+					
+					<td>
+						<?= $category['id'] ?>
+					</td>
+					<td>
+						<?= $category['name'] ?>
+					</td>
+					<td>
+						<?= $category['description'] ?>
+					</td>
+					<td>
+						<button onclick="edit(<?= $category['id'] ?>,'<?= $category['name'] ?>','<?= $category['description'] ?>','<?= $category['status'] ?>')">
+							Edit category
+						</button>
+
+						<button onclick="remove(<?= $category['id'] ?>)" style="background-color: red;color:white;">
+							Delete category
+						</button>
+					</td>
+
+				</tr>
+					
+				<?php endforeach  ?>
+				
+				<?php   
+
+				// foreach ($categories as $category) {
+					
+				// 	echo "<tr>
+					
+				// 		<td>
+				// 			".$category['id']."
+				// 		</td>
+				// 		<td>
+				// 			".$category['name']."
+				// 		</td>
+				// 		<td>
+				// 			".$category['description']."
+				// 		</td>
+
+				// 		<td>
+				// 			<button onclick='edit(".$category['id'].",\"".$category['name']."\",\"".$category['description']."\",\"".$category['status']."\")' >
+				// 				Edit category
+				// 			</button>
+
+				// 			<button onclick='delete(".$category['id'].")' > Delete category </button>
+
+				// 		</td>
+
+				// 	</tr>";
+
+				// }
+
+
+				?> 
+
+			</tbody>
+		</table>
+
+		<form id="storeForm" action="<?= BASE_PATH ?>/category" method="POST">
+			<fieldset>
+				
+				<legend>
+					Add new Category
+				</legend>
+
+				<label>
+					Name
+				</label>
+				<input type="text"  name="name" placeholder="terror" required=""> 
+				<br>
+
+				<label>
+					Description
+				</label>
+				<textarea placeholder="write here" name="description" rows="5" required=""></textarea>
+				<br>
+
+				<label>
+					Status
+				</label>
+				<select name="status">
+					<option> active </option>
+					<option> inactive </option>
+				</select>
+				<br>
+
+				<button type="submit" >Save Category</button>
+				<input type="hidden" name="action" value="store">
+				<input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
+
+			</fieldset>
+		</form>
+
+		<form id="updateForm" action="../category" method="POST">
+			<fieldset>
+				
+				<legend>
+					Edit Category
+				</legend>
+
+				<label>
+					Name
+				</label>
+				<input type="text" id="name" name="name" placeholder="terror" required=""> 
+				<br>
+
+				<label>
+					Description
+				</label>
+				<textarea placeholder="write here" id="description" name="description" rows="5" required=""></textarea>
+				<br>
+
+				<label>
+					Status
+				</label>
+				<select id="status" name="status">
+					<option> active </option>
+					<option> inactive </option>
+				</select>
+				<br>
+
+				<button type="submit" >Save Category</button>
+				<input type="hidden" name="action" value="update">
+				<input type="hidden" name="id" id="id">
+
+			</fieldset>
+		</form>
+
+		<form id="destroyForm" action="../category" method="POST">
+
+			<input type="hidden" name="action" value="destroy">
+			<input type="hidden" name="id" id="id_destroy">
+			<input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
+
+		</form>
+
+	</div>
+	<script type="text/javascript">
+		function edit(id,name,description,status)
+		{	
+			document.getElementById('storeForm').style.display="none";
+			document.getElementById('updateForm').style.display="block";
+
+			document.getElementById('name').value = name;
+			document.getElementById('description').value = description;
+			document.getElementById('status').value = status;
+			document.getElementById('id').value = id;
+		}
+
+		function remove(id)
+		{
+			var confirm = prompt("Si quiere eliminar el registro, escriba :"+id);
+			if (confirm == id) {
+
+				document.getElementById('id_destroy').value = id;
+				document.getElementById('destroyForm').submit();
+
+			}
+		}
+	</script>
+</body>
+</html>
